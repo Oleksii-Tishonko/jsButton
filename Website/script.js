caa6dd1aa70975bde7353bea28770b170d6cbd2a
Oleksii Tishonko <OleksiiTishonko@gmail.com>
2023-10-27 19:36:45 -0600

    buttonHandler

--- Website/script.js
+++ Website/script.js
@@ -1,1 +1,17 @@
 console.log("👋 🌎");
+
+const myElement = document.getElementById("root");
+
+let button = document.createElement("button");
+button.innerText = "click me";
+
+myElement.appendChild(button);
+button.classList.add("square");
+
+console.log(myElement);
+
+button.setAttribute("onClick", "buttonHandler()");
+
+function buttonHandler(){
+    console.dir(myElement);
+}
